7388e124d694621caa2afd87292c53a01742fb58
Vitaliy Klimov <vitalik_klimov@ukr.net>
2019-05-11 15:58:52 +0300

    final

---
+++ Source.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <string.h>
+
+char* TheLongestWord(char sentence[]);
+void VariabicFunction(int num, char first[], ...);
+
+int main() {
+	char arr[80];
+	char arr2[40];
+	gets(arr);
+	gets(arr2);
+	VariabicFunction(2, arr, arr2);
+
+	system("pause");
+	return 0;
+}
+
+void VariabicFunction(int num, char first[], ...)
+{
+	char **p1 = &first;
+	for (int i = 0; i < num; i++, p1++)
+	{
+		printf("in the sentence # %d the longest word is: %s \n", i, TheLongestWord(*p1));
+	}
+}
+
+char* TheLongestWord(char sentence[])
+{
+	const char *limits = " ,.;()-";
+	char *pw;
+	char *longest_word = "\0";
+	char *next_token1 = NULL;
+
+	pw = strtok_s(sentence, limits, &next_token1);
+	while (pw != NULL)
+	{
+		if (strlen(pw) > strlen(longest_word))
+		{
+			longest_word = pw;
+		}
+		pw = strtok_s(NULL, limits, &next_token1);
+	}
+	return longest_word;
+}
+
